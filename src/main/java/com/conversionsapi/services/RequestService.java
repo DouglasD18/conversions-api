619fb89bb7374954a63d5e9912a3abb1437178b8
Douglinhas <daguiaralcantara@gmail.com>
2023-11-20 17:00:19 -0300

    feat: create request service

---
+++ src/main/java/com/conversionsapi/services/RequestService.java
@@ -0,0 +1,31 @@
+package com.conversionsapi.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class RequestService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Float convert(String realCurr, String convertedCurr) throws Exception {
+        String url = "https://economia.awesomeapi.com.br/last/";
+        ResponseEntity<Map> response = restTemplate.getForEntity(url + realCurr.toUpperCase() + "-" + convertedCurr.toUpperCase(), Map.class);
+
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new Exception(String.valueOf(Objects.requireNonNull(response.getBody()).get("message")));
+        }
+
+        Map conversion = (Map) Objects.requireNonNull(response.getBody()).get(realCurr.toUpperCase() + convertedCurr.toUpperCase());
+        return (Float) conversion.get("bid");
+    }
+
+}
