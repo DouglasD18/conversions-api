1cb76993f97fa967d8c5f28a8529a546fd435cb7
Douglinhas <daguiaralcantara@gmail.com>
2023-11-20 15:57:02 -0300

    feat: create conversion domain

---
+++ src/main/java/com/conversionsapi/domain/conversion/Conversion.java
@@ -0,0 +1,27 @@
+package com.conversionsapi.domain.conversion;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="conversions")
+@Table(name="conversions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Conversion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal realValue;
+    private String realCurr;
+    private BigDecimal convertedValue;
+    private String convertCurr;
+    private LocalDateTime createdAt;
+
+}
