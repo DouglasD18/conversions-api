f22dc2ad2c56b7577a638f661e86a9c59a7d790c
Douglinhas <daguiaralcantara@gmail.com>
2023-11-20 15:42:50 -0300

    initial commit

---
+++ src/test/java/com/conversionsapi/ConversionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversionsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
